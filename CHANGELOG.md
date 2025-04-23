05557f19537cab3f113049c76c2a63c6421fab86
JavaCommons Technologies <javacommons@gmail.com>
2025-04-24 03:32:10 +0900

    yaml_io: toYaml() does not quote string value any more

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 2025.420.2340
 
 - Initial release
+
+## 2025.424.331
+
+- toYaml() does not quote string value any more
--- lib/yaml_io.dart
+++ lib/yaml_io.dart
@@ -5,13 +5,13 @@ import 'dart:convert' as convert;
 
 /// Converts a yaml string to a dart dynamic
 dynamic fromYaml(String $yaml) {
-  var doc = loadYaml($yaml);
-  return convert.json.decode((convert.json.encode(doc)));
+  var $doc = loadYaml($yaml);
+  return convert.json.decode((convert.json.encode($doc)));
 }
 
 /// Converts a dart dynamic to a yaml string
 String toYaml(dynamic $x) {
-  final yamlWriter = YamlWriter();
-  final yamlDoc = yamlWriter.write($x);
-  return yamlDoc.toString();
+  final $yamlWriter = YamlWriter(allowUnquotedStrings: true);
+  final $yamlDoc = $yamlWriter.write($x);
+  return $yamlDoc.toString();
 }
